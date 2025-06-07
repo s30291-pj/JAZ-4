a192c4d6c78f2f7c1d830da539741edd5159d373
s30291-pj <s30291@pjwstk.edu.pl>
2025-06-07 16:49:27 +0200

    update

--- src/main/java/pl/edu/pjwstk/s30291/projects/svc/movie/Movie.java
+++ src/main/java/pl/edu/pjwstk/s30291/projects/svc/movie/Movie.java
@@ -3,13 +3,14 @@ package pl.edu.pjwstk.s30291.projects.svc.movie;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -17,27 +18,41 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Schema(description = "Movie object used for basic movies control.")
 public class Movie {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Schema(description = "Unique identifier of the movie.<br><b>(must be null during data creation)</b>", example = "d997ae27-0a0a-439d-a982-c37279aa0f39")
 	private UUID id;
 	
+	@NotBlank
 	@Setter 
+	@Schema(description = "Date of movie publication")
 	private LocalDate published;
 	
+	@NotBlank
 	@Setter 
+	@Schema(description = "Title of the movie")
 	private String title;
 	
+	@NotBlank
 	@Setter 
+	@Schema(description = "Description of the movie")
 	private String description;
 	
-	@Setter 
+	@NotBlank
+	@Setter
+	@Schema(description = "Category of the movie")
 	private String category;
 	
+	@NotBlank
 	@Setter 
+	@Schema(description = "Author of the movie")
 	private String author;
 	
+	@NotBlank
 	@Setter
+	@Schema(description = "Availability status of the movie")
 	private boolean available;
 }
--- src/main/java/pl/edu/pjwstk/s30291/projects/svc/movie/controller/MovieController.java
+++ src/main/java/pl/edu/pjwstk/s30291/projects/svc/movie/controller/MovieController.java
@@ -13,11 +13,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import pl.edu.pjwstk.s30291.projects.svc.movie.Movie;
 import pl.edu.pjwstk.s30291.projects.svc.movie.service.MovieService;
 
+@OpenAPIDefinition(
+		info = @Info(
+			title = "Movie Service", 
+			description = "This is a movie service API that can be used to fetch/create/update movies as also change specific parameters like availability."
+			)
+		)
+
 @RestController
 @RequestMapping("/movies")
+@Tag(name = "Basic Operations - Movie Controller", description = "This controller provides basic data control.")
 public class MovieController {
 	private MovieService service;
 	
@@ -25,26 +42,66 @@ public class MovieController {
 		this.service = service;
 	}
 	
+	@Operation(summary = "Fetch all movies")
+	@ApiResponses(value = { 
+	  @ApiResponse(responseCode = "200", description = "Successfuly fetched all movies", 
+	    content = { @Content(mediaType = "application/json", 
+	      array = @ArraySchema(schema = @Schema(implementation = Movie.class))) })
+	})
 	@GetMapping
 	public ResponseEntity<List<Movie>> getAll() {
 		return ResponseEntity.ok(service.getAll());
 	}
 	
+	@Operation(summary = "Fetch movie with ID")
+	@ApiResponses(value = { 
+	  @ApiResponse(responseCode = "200", description = "Successfuly fetch movie with provided ID", 
+	    content = { @Content(mediaType = "application/json", 
+	      schema = @Schema(implementation = Movie.class)) }),
+	  @ApiResponse(responseCode = "400", description = "Invalid ID provided", 
+	    content = @Content), 
+	  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+	    content = @Content) })
 	@GetMapping("/{id}")
 	public ResponseEntity<Movie> getById(@PathVariable UUID id) {
 		return ResponseEntity.of(service.getById(id));
 	}
 	
+	@Operation(summary = "Create movie")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "200", description = "Movie stored in database", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid request provided", 
+			    content = @Content)})
 	@PostMapping
 	public ResponseEntity<Movie> create(@RequestBody Movie movie) {
 		return ResponseEntity.ok(service.create(movie));
 	}
 	
+	@Operation(summary = "Update movie with ID")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "200", description = "Movie updated successfuly", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid ID/request provided", 
+			    content = @Content), 
+			  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+			    content = @Content) })
 	@PutMapping("/{id}")
 	public ResponseEntity<Movie> update(@PathVariable UUID id, @RequestBody Movie movie) {
 		return ResponseEntity.ok(service.update(id, movie));
 	}
 	
+	@Operation(summary = "Delete movie with provided ID")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "204", description = "Movie deleted successfuly", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid ID provided", 
+			    content = @Content), 
+			  @ApiResponse(responseCode = "404", description = "Movie with provided ID not found", 
+			    content = @Content) })
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable UUID id) {
 		service.delete(id);
@@ -52,6 +109,15 @@ public class MovieController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@Operation(summary = "Change movie with provided ID availability")
+	@ApiResponses(value = { 
+			  @ApiResponse(responseCode = "200", description = "Movie availability changed successfuly", 
+			    content = { @Content(mediaType = "application/json", 
+			      schema = @Schema(implementation = Movie.class)) }),
+			  @ApiResponse(responseCode = "400", description = "Invalid ID provided", 
+			    content = @Content), 
+			  @ApiResponse(responseCode = "404", description = "Movie with provied ID not found", 
+			    content = @Content) })
 	@PostMapping("/{id}/availability")
 	public ResponseEntity<Movie> setAvailability(@PathVariable UUID id, @RequestBody boolean value) {
 		service.setAvailability(id, value);
